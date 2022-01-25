b9ee516bec03402dcf356ee338eb69f0d3472898
hojun21122 <hojun21122@gmail.com>
2022-01-25 17:58:57 -0500

    changed few menu features

--- src/Component/Header/header.js
+++ src/Component/Header/header.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react'
 import styles from './header.module.css'
 import logo from '../../image/logo_backgroundless.png'
-import { Container } from '@material-ui/core';
+import { Container } from '@material-ui/core'
+import Grid from '@mui/material/Grid';
+
 function Header(props){  
         return (                    
           <div className={styles.header}>
@@ -10,17 +12,13 @@ function Header(props){
             </div>
             <Container>
               <nav className={styles.navbar}>
-                <div className={styles.navbar_logo}>
-                  <a href="/"><img src={logo} alt="logo" width="100px"  /></a>           
-                </div>
                 <ul className={styles.navbar_menu}>
                     <li><a href="/">Home</a></li>
                     <li><a href="/myp">My Page</a></li>
+                    <li><a href="/"><img className={styles.logo} src={logo} alt="logo"/></a>    </li>
                     <li><a href="/about">About Me</a></li>                   
                     <li><a href="/corona">Corona</a></li> 
-
                 </ul>
-
               </nav> 
             </Container>
             
