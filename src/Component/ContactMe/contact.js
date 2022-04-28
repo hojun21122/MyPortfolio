1d6a4d2f633b7b1a772a3261937b020830a73a60
hojun21122 <hojun21122@gmail.com>
2022-04-28 02:21:04 -0400

    set up for email form

---
+++ src/Component/ContactMe/contact.js
@@ -0,0 +1,23 @@
+import React, {  Component, useState } from "react";
+import './contact.css'
+import { Container } from "@material-ui/core";
+import { useEffect, useRef} from "react";
+
+/*
+<a>linkedin.com/in/hojunhwangHJ</a><br></br>
+                        <a>647-550-9650</a><br></br>
+                        <a>hojun21122@gmail.com</a><br></br>
+*/
+
+
+
+
+function Contact (props) {
+        return (    
+            <Container fixed>
+               
+            </Container>      
+        );
+}
+
+export default Contact; 
