6aaf393661cb3108d655e77c60b308cac7fa8279
hojun21122 <hojun21122@gmail.com>
2022-06-17 09:20:20 -0400

    changing design

--- src/Component/About/about.js
+++ src/Component/About/about.js
@@ -2,147 +2,84 @@ import React  from "react";
 import './about.css'
 import { Container } from "@material-ui/core";
 import profile_pic from '../../image/profilePic.jpg'
-import gmm from '../../image/gmm.png'
+
+import korea from '../../image/korea.png'
 import seneca from '../../image/seneca.png'
 import machi from '../../image/machi.png'
-import seaMission from '../../image/sea mission.png'
+
+import img15 from '../../image/gallery/IMG_9230.webp'
 import myskill from '../../image/programming.jpg'
 
-function FadeInSection(props) {
-    const [isVisible, setVisible] = React.useState(false);
-    const domRef = React.useRef();
-    React.useEffect(() => {
-      const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => setVisible(entry.isIntersecting));
-      });
-      observer.observe(domRef.current);
-    }, []);
-    return (
-      <div
-      className={`about-fade-in ${isVisible ? 'is-visible' : ''}`} 
-      ref={domRef}
-      >
-        {props.children}
-      </div>
-    );
-  }
-function About(props) {  
-    /*
-    const containerRef = useRef();
-    const [isVisible, setisVisible] = useState(true);
-    useEffect(() =>{
-        const observer = new IntersectionObserver(entries =>{
-            entries.forEach(entry => setisVisible(entry.isIntersecting))
-        });
-        observer.observe(containerRef.current);
-        return() => observer.unobserve(containerRef.current);
-    },)*/
+function About() {  
         return (
-            <div className="div_background">
+          
+          <div className="about_background">
                 <Container>
-                <div className="about_background">
-                    <FadeInSection>
-                        <div>
-                            <div className="home_background">
-                                <div className="info">
-                                    <h1>My name is Ho Jun Hwang!</h1>
-                                    <p> I am a self-motivated, hard-working, individual
-                                        aspiring to a fulfilling career in information
-                                        technology services</p>
-                                </div>
-                                <div className="img_frame">
-                                <img src={profile_pic} className="profilePic" alt="profile"></img>
-                            </div>
-                            </div>  
+                  <div className="about_field">
+                 
+                        <div className="left_field">
+                          <div className="card_section_left">
+                              <div className="img_frame">
+                                  <img src={profile_pic} className="profilePic" alt="profile"></img>
+                              </div>
+                                  <div className="info">
+                                      <h1>I am Ho-Jun</h1>
+                                      <p> I am a self-motivated, hard-working, individual
+                                          aspiring to a fulfilling career in information
+                                          technology services</p>
+                                  </div>
+                          </div>
                         </div>
-                    </FadeInSection>
-                    <FadeInSection>
-                        <div  className="home_background" >
-                        <div className="info">
-                            <h1>Work Experience</h1>
-                                <h2>■ Good Music Ministry</h2>
-                                <h3> - Media & Project Director (2016.09 - Current)</h3>
-                                <p> ㅇ Responsible for managing and designing website using
-                                    Squarespace and managed
-                                    donators data with excel files. Check out our website!</p>
-                                <a href="https://www.goodmusicministry.org/" className="link" target="blank" > (https://www.goodmusicministry.org/)</a>
-                                <p>ㅇ Project planning and managing cultural events with more than 100 people</p>
-                                <p>ㅇ Reaching out to donors to summarize and provide activities
-                                    done with donors donations.</p>
-                                <p>ㅇ Built team communication plans and manage project schedules
-                                    that's suitable for all team members</p>
-                                <p>ㅇ Recognized for superior customer service and teamwork</p>
-                                <p>ㅇ Demonstrated self-motivation by proactively researching
-                                        Adobe CC, video editing, filming, photography and
-                                        self taught skills as required</p>
-                            </div>
-                            <div className="img_frame">
-                                <img src={gmm} className="profilePic" alt="gmm logo"></img>
-                            </div>
+                        
+                        <div className="right_field">
+                          <div className="card_section_right">
+                              <div className="img_frame">
+                                      <img src={seneca} className="seneca" alt="seneca logo"></img>
+                                  </div>
+                                  <div className="info"> 
+                                      <h1>Education</h1>
+                                      <p>Seneca College Computer Programming & Analysis (CPA)<br></br>
+                                      Ontario College Advanced Diploma</p>
+                                  </div>
+                          </div>
                         </div>
-                    </FadeInSection>
-                    <FadeInSection>
-                        <div  className="home_background" >
-                        <div className="info">
-                                <h2>■ MachiMachi Canada</h2>
-                                <h3> - Kitchen Staff (2020.02 - Current)</h3>
-                                <p> ㅇ Experience working under pressure in a fast-paced environment</p>
-                                <p>ㅇ Leverage communication skills to build strong relationships with coworkers</p>
-                                <p>ㅇ Able to demonstrate management capabilities and oversight required to manage store inventory</p>
-                                <p>ㅇ Able to prioritize work and can quickly adapt to ad-hoc requests</p>
-                            </div>
-                            <div className="img_frame">
-                                <img src={machi} className="profilePic" alt="machi logo"></img>
-                            </div>
+                        <div className="right_field">
+                          <div className="card_section_right2">
+                              <div className="img_frame">
+                                      <img src={myskill} className="skills" alt="seneca logo"></img>
+                                  </div>
+                                  <div className="info"> 
+                                      <h1>Skills</h1>
+                                      <p>C++, Java, JavaScript, Python, Html, Css, SQL, React, Angular, MongoDb, Node, AdobeCC</p>
+                                  </div>
+                          </div>
                         </div>
-                    </FadeInSection>
-                    <FadeInSection>
-                        <div  className="home_background" >
-                        <div className="info">
-                                <h2>■ SEA Mission INC</h2>
-                                <h3> - Project Coordinator</h3>
-                                <p> ㅇ Coordinated donation projects and programs in a non-profit
-                                    organization to serve low-income families especially near the
-                                    finch&jane area.</p>
-                                <p>ㅇ Built connection between community local stores and company.</p>
-                                <p>ㅇ Project planning and managing for helping low income family that helps more than 60 people.</p>
-                                <p>ㅇ Created monthly newsletter about SEA MISSION’s volunteer program</p>
-                                
-                            </div>
-                            
-                            <div className="img_frame">
-                                <img src={seaMission} className="profilePic" alt="SEA mission logo"></img>
-                            </div>
+                        <div className="left_field">
+                          <div className="card_section_left2">
+                              <div className="img_frame">
+                                  <img src={korea} className="profilePic" alt="profile"></img>
+                              </div>
+                                  <div className="info">
+                                      <h1>I am an Immigrant</h1>
+                                      <p> I Immigrated from Korea to Canada in 2012 which allow me to  speak Korean and English fluently.</p>
+                                  </div>
+                          </div>
                         </div>
-                    </FadeInSection>
-                    <FadeInSection>
-                        <div className="home_background">
-                            <div className="info"> 
-                            <h1>My Skills</h1>
-                                <p> I am programmable with C/C++, C#, Java, Python, JavaScript, Html,CSS, Swift, Objective C </p>
-                            </div>
-                            <div className="img_frame">
-                                <img src={myskill} className="seneca" alt="programming logo"></img>
-                            </div>
+                        <div className="left_field">
+                          <div className="card_section_left3">
+                              <div className="img_frame">
+                                  <img src={img15} className="seneca" alt="profile" style={{filter: 'brightness(95%) contrast(85%) grayscale(50%)'}}></img>
+                              </div>
+                                  <div className="info">
+                                      <h1>I am Photographer / Videographer</h1>
+                                      <p> I like to record my journey as Photo/Video. It improves my creativity and imagination for my designs and project ideas.</p>
+                                  </div>
+                          </div>
                         </div>
-                    </FadeInSection>
-                    <FadeInSection>
-                         <div className="home_background">
-                            <div className="info"> 
-                                <h1>Education</h1>
-                                <p>Seneca College Computer Programming & Analysis (CPA)<br></br>
-                                  Ontario College Advanced Diploma</p>
-                            </div>
-                            <div className="img_frame">
-                                <img src={seneca} className="seneca" alt="seneca logo"></img>
-                            </div>
-                           
-                        </div> 
-                    </FadeInSection>
-                   
-                </div>
+                    
+                  </div>
             </Container>
-            </div>         
+          </div>
         );
 }
 
--- src/Component/AboutMe/aboutMe.js
+++ src/Component/AboutMe/aboutMe.js
@@ -10,47 +10,7 @@ import "react-image-gallery/styles/css/image-gallery.css";
 
 function AboutMe(props){
     return(
-        <div className="aboutMe">
-            <Container>
-            <div className="journey">
-                <div className="korea">
-                    <img src={korea} className="korea-img" alt="Korea"></img>
-                </div>
-                <div id="arrowAnim">
-                        <div class="arrowSliding">
-                            <div class="arrow"></div>
-                        </div>
-                        <div class="arrowSliding delay1">
-                            <div class="arrow"></div>
-                        </div>
-                        <div class="arrowSliding delay2">
-                            <div class="arrow"></div>
-                        </div>
-                        <div class="arrowSliding delay3">
-                            <div class="arrow"></div>
-                        </div>
-                    </div>
-                <div className="Canada">
-                    <img src={canada} className="canada-img" alt="canada"></img>
-                </div>
-            </div>
-            <div className="first_para">
-                <p>My name is Ho Jun Hwang and I was born in Seoul, Korea.<br></br>
-                I immigrated to Canada when I was 16 years old.  </p>
-            </div>
-        </Container>
-        <div className="video-container">
-            <video autoPlay muted loop>
-                <source src={helloWorld}></source>
-            </video>
-            <Container className="caption">
-                <div className="captionbox">
-                    <h1>I am programmer!</h1>
-                        <p><br></br>studied in Seneca College with Computer Programming and Analysist(CPA) program.<br></br>
-                    And I am a person who loves challenges and problem solving </p>
-                </div>
-            </Container>
-        </div>
+        <div>
         <Container>
             <div className="interest">
                 
@@ -58,7 +18,6 @@ function AboutMe(props){
                 <p>You can take a look at my media projects here!</p>
             </div>
             <div className="grid">
-             <ImageGallery className="gallery" items={images} lazyLoad="true"/>
              <div className="video-grid">
                  <iframe className="top-video" src="https://www.youtube.com/embed/_bTyfp3YWg8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ="true"></iframe>
                 <iframe className="top-video" src="https://www.youtube.com/embed/f7yn5_Istnc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ="true"></iframe>
