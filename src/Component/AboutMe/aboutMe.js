96019c844dee042ca5edb0f62bca3b00e5f35a50
hojun21122 <hojun21122@gmail.com>
2022-07-13 14:42:29 -0400

    adding meta tag

--- src/Component/AboutMe/aboutMe.js
+++ src/Component/AboutMe/aboutMe.js
@@ -1,11 +1,6 @@
 import React from "react";
 import './aboutMe.css'
 import { Container } from "@material-ui/core";
-import korea from '../../image/korea.png'
-import canada from '../../image/canada.jpg'
-import helloWorld from '../../image/helloWorld.mp4'
-import  ImageGallery  from "react-image-gallery";
-import images from './images'
 import "react-image-gallery/styles/css/image-gallery.css";
 
 function AboutMe(props){
@@ -17,13 +12,11 @@ function AboutMe(props){
                 <h1>I am a photographer and videographer</h1>
                 <p>You can take a look at my media projects here!</p>
             </div>
-            <div className="grid">
-             <div className="video-grid">
-                 <iframe className="top-video" src="https://www.youtube.com/embed/_bTyfp3YWg8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ="true"></iframe>
-                <iframe className="top-video" src="https://www.youtube.com/embed/f7yn5_Istnc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ="true"></iframe>
-                <iframe className="bottom-video" src="https://www.youtube.com/embed/1eor27GD4jA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen ="true"></iframe>
-                <iframe className="bottom-video" src="https://www.youtube.com/embed/BKK7wT6_V8U" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="true"></iframe>
-             </div>
+            <div className="video-grid">
+                 <iframe className="left-video" src="https://www.youtube.com/embed/_bTyfp3YWg8" title="YouTube video player" frameborder="0" allowfullscreen ="true"></iframe>
+                <iframe className="right-video" src="https://www.youtube.com/embed/f7yn5_Istnc" title="YouTube video player" frameborder="0" allowfullscreen ="true"></iframe>
+                <iframe className="left-video" src="https://www.youtube.com/embed/1eor27GD4jA" title="YouTube video player" frameborder="0"  allowfullscreen ="true"></iframe>
+                <iframe className="right-video" src="https://www.youtube.com/embed/BKK7wT6_V8U" title="YouTube video player" frameborder="0" allowfullscreen="true"></iframe>
             </div>
                 
         </Container>
